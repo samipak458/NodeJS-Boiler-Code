9069c47f12ff309e4a8bf187b203f9a5e205dd11
samipak458 <samipak458@gmail.com>
2023-10-05 10:40:08 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import data from "./data/mock.json" assert { type: "json" };
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Currently, Server running on port ${PORT}`);
+    console.log(data);
+    });
